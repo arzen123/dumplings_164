976feeaa144812f3fd3781a1a4a832861899e0ff
arzen123 <91918008+arzen123@users.noreply.github.com>
2022-02-02 16:43:56 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,73 +1,109 @@
-from cProfile import label
 from tkinter import *
 from tkinter import messagebox
+from tkinter.ttk import Combobox  
 
-colorfulnesses = [("1+0", 1), ("1+1", 2), ("4+0", 3), ("4+4", 4)]
-sizes = [("a4", 1), ("a5", 2), ("a6", 3), ("другой", 4)]
-papers = ["картон мат", "картон мел", "мел мат", "мел глян", "этикеточная", "офсет"]
-ofcet_densitys= [65, 70, 80, 100, 120, 190]
-mel_densitys = [90, 115, 130, 150, 200, 250, 300]
-kar_mel_densitys = [215, 235, 250, 270, 295, 325]
-kar_mat_densitys = [230, 250, 280, 300, 320, 350, 400]
-a3 = 720 * 520
+colorfulnesses = ["1+0", "1+1", "4+0", "4+4"]
+a3 = 350 * 420
+papers = [("картон мат", 230), ("картон мат", 250), ("картон мат", 280), ("картон мат", 300), ("картон мат", 320), ("картон мат", 350),
+        ("картон мел", 215), ("картон мел", 235), ("картон мел", 250), ("картон мел", 270), ("картон мел", 295), ("картон мел", 300),
+        ("мел мат", 90), ("мел мат", 115), ("мел мат", 130), ("мел мат", 150), ("мел мат", 170), ("мел мат", 200), ("мел мат", 250), ("мел мат", 300), ("мел мат", 350),
+        ("мел глян", 90), ("мел глян", 115), ("мел глян", 130), ("мел глян", 150), ("мел глян", 170), ("мел глян", 200), ("мел глян", 250), ("мел глян", 300), ("мел глян", 350),
+        ("этикеточная", 1),
+        ("офсет", 65), ("офсет", 70), ("офсет", 80), ("офсет", 100), ("офсет", 120), ("офсет", 190)]
 
 def clear():
     circulation_entry.delete(0, END)
 
+def prise(message):
+    messagebox.showinfo("GUI Python", message)
+
 def display():
-    messagebox.showinfo("GUI Python", colorfulness.get())
+    circulations = circulation.get()
+    colorfulness1, colorfulness2 = select1()
+    size = select2()
+    number_of_porduction = int(a3 / size)
+    number_of_paper = int(circulations / number_of_porduction)
+    
+    list = []
+    if paper.get() == "этикеточная 1" or paper.get().startswith('офсет'):
+        for word in paper.get().split():
+            list.append(word)
+    else:
+        for word in paper.get().split(','):
+            for word in word.split('{'):
+                for word in word.split('}'):
+                    list.append(word)
+
+    d = list[1]
+    p = list[0]
+    d = int(d)
+    print(d, p)
+    price_of_paper, mortgage =select3(number_of_paper, d, p)
+    
+    if colorfulness1 == 2:
+        if colorfulness2 == 0:
+            dye = float((number_of_paper * 0.2) / 1000.0)
+        else:
+            colorfulness2 == 2
+            dye = float((number_of_paper * 0.2) * 2 / 1000.0)
+    else:
+        colorfulness1 == 4
+        if colorfulness2 == 0:
+            dye = float((number_of_paper * 0.35) / 1000)
+        else:
+            colorfulness2 == 4
+            dye = float((number_of_paper * 0.35) * 2 / 1000)
+
+    price_of_dye = float(dye * 4.84)
+    matirial = price_of_dye + price_of_paper
+    
+
+#Прилада
+    if number_of_paper <= 5000:
+       price_makeready = 450
+    else:
+        makeready = float(2 + (5000 / (number_of_paper - 5000)))
+        price_makeready = makeready * 180.00
+    
+    
+    one_cut = number_of_paper / mortgage
+    cut = float((((number_of_porduction * one_cut) + (4 * one_cut)) * 0.3 + 30) / 60)
+    price_cut = float(3 * cut)
+    price_package = float((cut / 2) * 1.5)
+
+#цена
+    price_of_add = float((matirial + price_makeready + price_cut + price_package) * 0.4)
+    nds = float((matirial + price_makeready + price_cut + price_package + price_of_add) * 0.2)
+    price = matirial + price_makeready + price_cut + price_package + price_of_add     
+    price_with_nds = matirial + price_makeready + price_cut + price_package + price_of_add + nds  
+    message = f'Стоимость материалов = {matirial}. Стоимость приладки = {price_makeready}. Цена резки = {price_cut} Цена упаковки = {price_package}. Цена с НДС = {price_with_nds}, цена без НДС = {price}'
+
+    prise(message)
 
 def select1():
     c = colorfulness.get()
-    if c == 1:
+    if c == '1+0':
         colorfulness1 = 2
         colorfulness2 = 0
-    elif c == 2:
+    elif c == "1+1":
         colorfulness1 = 2
         colorfulness2 = 2
-    elif c == 3:
+    elif c == "4+0":
         colorfulness1 = 4
         colorfulness2 = 0
-    elif c == 4:
+    elif c == "4+4":
         colorfulness1 = 4
         colorfulness2 = 4
     return colorfulness1, colorfulness2,
-
-
+    
 def select2():
-    s = size.get()
-    if s == 1:
-        size1 = 210
-        size2 = 297
-    elif s == 2:
-        size1 = 148
-        size2 = 210
-    elif s == 3:
-        size1 = 105
-        size2 = 148
-    elif s == 4:
-        size1 = IntVar()
-        size2 = IntVar()
+    size = size1.get() * size2.get()
+    return size
 
-        size1_label = Label(text="по высоте")
-        size2_label = Label(text="по шрине")
 
-        size1_label.grid(row=6, column=5, sticky="w")
-        size2_label.grid(row=7, column=5, sticky="w")
-
-        size1_entry = Entry(textvariable=size1)
-        size2_entry = Entry(textvariable=size2)
-        size1_entry.grid(row=6, column=6, sticky="w")
-        size2_entry.grid(row=7, column=6, sticky="w")
-    return size1, size2
-
-def select3(density):
-    p = paper.get()
-    d = 1
+def select3(number_of_paper, d, p):
     if p == "офсет":
-        
-        drop2 = OptionMenu( root , density , *ofcet_densitys)
-        drop2.grid(row=7, column=1, padx=15, pady=10)
+        print(p)
         if d == 65:
             price_of_paper = number_of_paper * 0.047
             mortgage = 1785
@@ -86,12 +122,11 @@ def select3(density):
         elif d == 190:
             price_of_paper = number_of_paper * 0.128
             mortgage = 681
+        print(d, price_of_paper, mortgage)
     elif p == "этикеточная":
         price_of_paper = number_of_paper * 0.114
         mortgage = 2142
     elif p == "мел глян":
-        drop2 = OptionMenu( root , density , *mel_densitys)
-        drop2.grid(row=7, column=1, padx=15, pady=10)
         if d == 90:
             price_of_paper = number_of_paper * 0.091
             mortgage = 2500
@@ -116,9 +151,10 @@ def select3(density):
         elif d == 300:
             price_of_paper = number_of_paper * 0.321
             mortgage = 681
+        elif d == 350:
+            price_of_paper = number_of_paper * 0.321
+            mortgage = 681
     elif p == "мел мат":
-        drop2 = OptionMenu( root , density , *mel_densitys)
-        drop2.grid(row=7, column=1, padx=15, pady=10)
         if d == 90:
             price_of_paper = number_of_paper * 0.09
             mortgage = 2307
@@ -143,9 +179,10 @@ def select3(density):
         elif d == 300:
             price_of_paper = number_of_paper * 0.321
             mortgage = 483
+        elif d == 350:
+            price_of_paper = number_of_paper * 0.321
+            mortgage = 483
     elif p == "картон мел":
-        drop2 = OptionMenu( root , density , *kar_mel_densitys)
-        drop2.grid(row=7, column=1, padx=15, pady=10)
         if d == 215:
             mortgage = 491
             price_of_paper = number_of_paper * 0.233
@@ -161,12 +198,10 @@ def select3(density):
         elif d == 295:
             mortgage = 294
             price_of_paper = number_of_paper * 0.37
-        elif d == 325:
+        elif d == 300:
             mortgage = 267
             price_of_paper = number_of_paper * 0.4
-    elif p == "картон мак":
-        drop2 = OptionMenu( root , density , *kar_mat_densitys)
-        drop2.grid(row=7, column=1, padx=15, pady=10)
+    elif p == "картон мат":
         if d == 230:
             mortgage = 526
             price_of_paper = number_of_paper * 0.233
@@ -185,130 +220,51 @@ def select3(density):
         elif d == 350:
             mortgage = 333
             price_of_paper = number_of_paper * 0.4
-        elif d == 400:
-            mortgage = 285
-            price_of_paper = number_of_paper * 0.42
-    
+    return price_of_paper, mortgage
+
 
 root = Tk()
 root.title("GUI на Python")
 root.geometry("500x500")
 
-density = IntVar()
-paper = StringVar()
-paper.set( "картон мат" )
+paper = Combobox(root)
+paper.set( "Выберите бумагу" )
+paper['value'] = papers
+paper.grid(column=0, row=2)  
 
-drop1 = OptionMenu( root , paper , *papers, command=select3)
-drop1.grid(row=7, column=0, padx=15, pady=10)
+size1 = IntVar()
+size2 = IntVar()
 
+size1_label = Label(text="по высоте")
+size2_label = Label(text="по шрине")
 
+size1_label.grid(row=3, column=0, sticky="w")
+size2_label.grid(row=5, column=0, sticky="w")
 
-header1 = Label(text="Выберите красочность", padx=15, pady=10)
-header2 = Label(text="Выберите формат", padx=15, pady=10)
-header1.grid(row=2, column=0, sticky=W)
-header2.grid(row=2, column=1, sticky=W)
+size1_entry = Entry(textvariable=size1)
+size2_entry = Entry(textvariable=size2)
+size1_entry.grid(row=4, column=0, sticky="w")
+size2_entry.grid(row=6, column=0, sticky="w")
 
-colorfulness = IntVar()
-size = IntVar()
-paper = StringVar()
-density = IntVar()
-
-
-row = 3
-for txt, val in colorfulnesses:
-    Radiobutton(text=txt, value=val, variable=colorfulness, padx=15, pady=10, command=select1)\
-        .grid(row=row, sticky=W)
-    row += 1
-
-sel1 = Label(padx=15, pady=10)
-sel1.grid(row=row, sticky=W)
-
-row = 3
-for txt, val in sizes:
-    Radiobutton(text=txt, value=val, variable=size, padx=15, pady=10, command=select2)\
-        .grid(row=row, column=1, sticky=W)
-    row += 1
-
-sel2 = Label(padx=15, pady=10)
-sel2.grid(row=row, column=1, sticky=W)
-
+colorfulness = Combobox(root)
+colorfulness.set( "Выберите карсочность" )
+colorfulness['value'] = colorfulnesses
+colorfulness.grid(column=0, row=7)  
 
 circulation = IntVar()
 
-circulation_label = Label(text="Тираж",padx=15, pady=10)
-circulation_label.grid(row=0, column=0, sticky="w")
+circulation_label = Label(text="Тираж")
+circulation_label.grid(row=0, column=0, sticky='w')
 
 circulation_entry = Entry(textvariable=circulation)
-circulation_entry.grid(row=1, column=0, sticky="w", padx=15)
+circulation_entry.grid(row=1, column=0, sticky='w')
 
 message_button = Button(text="Посчитать", command=display)
 message_button2 = Button(text="Отчистить", command=clear)
 
-message_button.grid(row=0, column=5, sticky="e")
-message_button2.grid(row=0, column=6, sticky="e")
 
+message_button.grid(row=0, column=5)
+message_button2.grid(row=0, column=6)
 
-while True:
-    #size = size1 * size2
-    colorfulness1 = int(input('Красочность первое значение - '))
-    colorfulness2 = int(input('Красочность второе значение - '))
-    paper = input('Бумага - ')
-    if paper.startswith("картон"):
-        paper_for_matirial = paper.split(" ")
-        paper_for_matirial = paper_for_matirial[0]
-    if not paper.startswith("этикеточная"):
-        density = int(input('Плотность бумаги -'))
-
-#Расчет количества бумаги   
-    number_of_porduction = int(a3 / size)
-    number_of_paper = int(circulation / number_of_porduction)
-
-
-
-
-#Расчет краски
-    if colorfulness1 == 2:
-        if colorfulness2 == 0:
-            dye = float((number_of_paper * 0.2) / 1000.0)
-        elif colorfulness2 == 2:
-            dye = float((number_of_paper * 0.2) * 2 / 1000.0)
-        else:
-            print(
-                'неправильно ввели вторую краскосочность, {colorfulness2} а надо 2 или 4')
-    elif colorfulness1 == 4:
-        if colorfulness2 == 0:
-            dye = float((number_of_paper * 0.35) / 1000)
-        elif colorfulness2 == 4:
-            dye = float((number_of_paper * 0.35) * 2 / 1000)
-        else:
-            print(
-                'неправильно ввели вторую краскосочность, {colorfulness2} а надо 2 или 4')
-    else:
-        print(
-            'неправильно ввели первую красочность, вы ввели {colorfulness1}, а надо 2 или 4')
-    price_of_dye = float(dye * 4.84)
-    matirial = price_of_dye + price_of_paper
-    print("Стоимость материалов", matirial)
-
-#Прилада
-    if number_of_paper <= 5000:
-       price_makeready = 450
-    else:
-        makeready = float(2 + (5000 / (number_of_paper - 5000)))
-        price_makeready = makeready * 180.00
-    print("Стоимость приладки = ", price_makeready)
-    
-    one_cut = number_of_paper / mortgage
-    cut = float((((number_of_porduction * one_cut) + (4 * one_cut)) * 0.3 + 30) / 60)
-    price_cut = float(3 * cut)
-    price_package = float((cut / 2) * 1.5)
-    print("Цена резки", price_cut, "Цена упаковки", price_package)
-
-#цена
-    price_of_add = float((matirial + price_makeready + price_cut + price_package) * 0.4)
-    nds = float((matirial + price_makeready + price_cut + price_package + price_of_add) * 0.2)
-    price = matirial + price_makeready + price_cut + price_package + price_of_add     
-    price_with_nds = matirial + price_makeready + price_cut + price_package + price_of_add + nds  
-    print(f'Цена с НДС = {price_with_nds}, цена без НДС = {price}')
-
-    root.mainloop()
+   
+root.mainloop()
